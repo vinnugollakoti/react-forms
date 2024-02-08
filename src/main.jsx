13fe39e7200a369e20243338c02c61b8f198171e
vinay reddy gollakoti <145451307+vinnugollakoti@users.noreply.github.com>
2024-02-08 16:54:39 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RegisterForm from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RegisterForm />
+  </React.StrictMode>,
+)
